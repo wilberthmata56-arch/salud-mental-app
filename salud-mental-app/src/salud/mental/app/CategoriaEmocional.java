b8ce840af4ed60b3c7e70aab6bb42c8ec27e75fd
StevenAriasNovoa <136620715+StevenAriasNovoa@users.noreply.github.com>
2026-07-28 20:22:15 -0600

    Avance 2: clase base CategoriaEmocional

---
+++ salud-mental-app/src/salud/mental/app/CategoriaEmocional.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salud.mental.app;
+
+/**
+ *
+ * @author arias
+ */
+public class CategoriaEmocional {
+
+    private String nombre;
+
+    public CategoriaEmocional() {
+    }
+
+    public CategoriaEmocional(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
