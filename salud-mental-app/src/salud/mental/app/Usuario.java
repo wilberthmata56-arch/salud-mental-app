9a66008047a44cb6b49adaba24f4646cfb1ee93b
StevenAriasNovoa <136620715+StevenAriasNovoa@users.noreply.github.com>
2026-07-28 20:18:18 -0600

    Avance 2: clase base Usuario

---
+++ salud-mental-app/src/salud/mental/app/Usuario.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salud.mental.app;
+
+/**
+ *
+ * @author arias
+ */
+public class Usuario {
+
+    private String nombre;
+    private int edad;
+    private String correo;
+    private String contrasena;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, int edad, String correo, String contrasena) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
